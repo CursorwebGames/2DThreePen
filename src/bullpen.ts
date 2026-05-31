1fac1856c8c4c664a01bff3cb7c8b324d3647701
Junhao Zhang <jerry_mountain@hotmail.com>
2026-05-30 19:40:58 -0500

    Everything is snappy border now

--- src/bullpen.ts
+++ src/bullpen.ts
@@ -6,6 +6,7 @@ const DOT = 1;
 
 // const RECT_SIZE = 50;
 const PADDING = 1;
+export const REGION_BORDER = 3;
 
 const MOBILE = true;
 
@@ -21,7 +22,8 @@ export class BullPen {
 
     private undoStack: Mask[][][] = [];
     private redoStack: Mask[][][] = [];
-    private _beforeSnapshot: Mask[][] | null = null;
+    /** Snapshot to keep track of undo stack */
+    private beforeSnapshot: Mask[][] | null = null;
 
     private dragMode: typeof DOT | typeof EMPTY | null = null;
     private hasDragged = false;
@@ -41,7 +43,7 @@ export class BullPen {
 7 7 7 7 1 1 1 1`.trim().split('\n').map(x => x.split(' ').map(Number));
         const size = this.board.length;
         this.size = size;
-        this.rectSize = 50 * 8 / this.size;
+        this.rectSize = (50 * 8) / this.size;
 
         this.mask = Array.from({ length: size }, () => Array(size).fill(EMPTY));
 
@@ -52,18 +54,25 @@ export class BullPen {
     }
 
     draw() {
-        noStroke();
+        push();
+        translate(REGION_BORDER, REGION_BORDER);
         textSize(this.rectSize - 10);
         textAlign(CENTER, CENTER);
         for (let y = 0; y < this.mask.length; y++) {
             for (let x = 0; x < this.mask[0].length; x++) {
-                const rx = (this.rectSize + PADDING / 2) * x;
-                const ry = (this.rectSize + PADDING / 2) * y;
+                const rx = this.rectSize * x;
+                const ry = this.rectSize * y;
 
                 const cellColor = this.colors[this.board[y][x]];
+                const darkColor = lerpColor(cellColor, color(0), 0.2);
 
+                noStroke();
+                fill(darkColor);
+                rect(rx, ry, this.rectSize, this.rectSize);
+                strokeWeight(1);
                 fill(cellColor);
-                rect(rx, ry, this.rectSize, this.rectSize, 2);
+                stroke(darkColor);
+                rect(rx, ry, this.rectSize, this.rectSize, 4);
 
                 const cell = this.mask[y][x];
                 if (cell == BULL) {
@@ -80,11 +89,41 @@ export class BullPen {
             }
         }
 
+
+        // border
+        for (let y = 0; y < this.mask.length; y++) {
+            for (let x = 0; x < this.mask[0].length; x++) {
+                const rx = this.rectSize * x;
+                const ry = this.rectSize * y;
+
+                stroke(0);
+                strokeWeight(REGION_BORDER);
+                const regionId = this.board[y][x];
+                if (x + 1 >= this.mask[0].length || this.board[y][x + 1] != regionId) {
+                    line(rx + this.rectSize, ry, rx + this.rectSize, ry + this.rectSize);
+                }
+
+                if (y + 1 >= this.mask.length || this.board[y + 1][x] != regionId) {
+                    line(rx, ry + this.rectSize, rx + this.rectSize, ry + this.rectSize);
+                }
+
+                if (x - 1 < 0 || this.board[y][x - 1] != regionId) {
+                    line(rx, ry, rx, ry + this.rectSize);
+                }
+
+                if (y - 1 < 0 || this.board[y - 1][x] != regionId) {
+                    line(rx, ry, rx + this.rectSize, ry);
+                }
+
+                noStroke();
+            }
+        }
+        pop();
     }
 
     private cellAt(mx: number, my: number): { x: number; y: number } | null {
-        const x = Math.floor(mx / (this.rectSize + PADDING / 2));
-        const y = Math.floor(my / (this.rectSize + PADDING / 2));
+        const x = Math.floor((mx - REGION_BORDER) / (this.rectSize + PADDING / 2));
+        const y = Math.floor((my - REGION_BORDER) / (this.rectSize + PADDING / 2));
         if (x >= 0 && x < this.mask[0].length && y >= 0 && y < this.mask.length) {
             return { x, y };
         }
@@ -130,7 +169,7 @@ export class BullPen {
         this.board = board;
         const size = this.board.length;
         this.size = size;
-        this.rectSize = 50 * 8 / this.size;
+        this.rectSize = (50 * 8 - 2 * REGION_BORDER) / this.size;
 
         this.mask = Array.from({ length: size }, () => Array(size).fill(EMPTY));
         this.colors = [];
@@ -168,7 +207,7 @@ export class BullPen {
 
     mousePressed() {
         this.hasDragged = false;
-        this._beforeSnapshot = this.copyMask();
+        this.beforeSnapshot = this.copyMask();
         const cell = this.cellAt(mouseX, mouseY);
         this.pressedCell = cell;
         if (!cell) return;
@@ -180,11 +219,11 @@ export class BullPen {
 
     mouseReleased() {
         if (this.hasDragged) {
-            this.pushUndo(this._beforeSnapshot!);
+            this.pushUndo(this.beforeSnapshot!);
         } else if (this.pressedCell) {
             const { x, y } = this.pressedCell;
-            const cur = this._beforeSnapshot![y][x];
-            this.mask = this._beforeSnapshot!.map(row => [...row]) as Mask[][];
+            const cur = this.beforeSnapshot![y][x];
+            this.mask = this.beforeSnapshot!.map(row => [...row]) as Mask[][];
             if (MOBILE) {
                 if (cur == EMPTY) this.mask[y][x] = DOT;
                 else if (cur == DOT) this.mask[y][x] = BULL;
@@ -196,7 +235,7 @@ export class BullPen {
             } else if (mouseButton.right) {
                 this.mask[y][x] = cur == BULL ? EMPTY : BULL;
             }
-            this.pushUndo(this._beforeSnapshot!);
+            this.pushUndo(this.beforeSnapshot!);
         }
         this.dragMode = null;
     }
